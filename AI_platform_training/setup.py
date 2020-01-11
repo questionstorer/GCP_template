55e1d96c6df1e6482417af2286e5e5de63472451
Jianing <hongmei3318@hotmail.com>
2020-01-11 12:44:09 +0800

    update demo

--- AI_platform_training/setup.py
+++ AI_platform_training/setup.py
@@ -2,8 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 REQUIRED_PACKAGES = [
-    'tensorflow==1.13.1',
-    'tensorflow-model-analysis==0.13.0',
+    'tensorflow==1.15.1',
     'nltk==3.4.5'
 ]
 
