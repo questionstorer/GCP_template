47e2e84869941d8ef119c7b2405b45b62710c38e
Jianing <hongmei3318@hotmail.com>
2020-01-14 15:58:40 +0800

    update demo prediction

--- AI_platform_prediction/setup.py
+++ AI_platform_prediction/setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup
+from setuptools import find_packages
+
+REQUIRED_PACKAGES = [
+    'tensorflow==1.14.0',
+    'nltk==3.4.5'
+]
 
 setup(
     name='GCP_demo_prediction',
     version='0.1',
-    scripts=['predictor.py'])
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=['predictor.py']),
