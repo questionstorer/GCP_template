e3cf24982ba97de05e1b4adf3fd571c19c92ff01
Jianing <hongmei3318@hotmail.com>
2020-01-14 14:38:30 +0800

    update demo prediction

---
+++ AI_platform_prediction/predictor.py
@@ -0,0 +1,24 @@
+from sklearn.externals import joblib
+import numpy as np
+import os
+
+class MyPredictor(object):
+  def __init__(self, model, encoder):
+    self._model = model
+    self._encoder = encoder
+
+  def predict(self, instances, **kwargs):
+    inputs = np.asarray(instances)
+    preprocessed_inputs = self._encoder.transform(inputs)
+    return self._model.predict(preprocessed_inputs)
+
+  @classmethod
+  def from_path(cls, model_dir):
+    model_path = os.path.join(model_dir, 'titanic.joblib')
+    model = joblib.load(model_path)
+
+    encoder_path = os.path.join(model_dir, 'input_encoder.pkl')
+    with open(encoder_path, 'rb') as f:
+      encoder = joblib.load(encoder_path)
+
+    return cls(model, encoder)
